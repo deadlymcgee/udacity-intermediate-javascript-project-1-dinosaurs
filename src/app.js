95439f0057fccedfbd7d0c42255b03cea590016b
npwaters <40352015+npwaters@users.noreply.github.com>
2021-05-24 18:14:58 +1000

    doc: Add function documentation relating to creation of Dinosaur objects

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import data from "./dino.json"
 import images from "./images/*.png"
 
 // TODO: Refactor using module reveal pattern
+/**
+ * @description Factory function that creates a Dinosaur object with shared methods and private attributes
+ * @param {Object} dino - An object representing a Dinosaur from a JSON file
+ * @returns {Object} - A Dinosaur object
+ */
 const dinoFactory = function(
     {species, weight, height, diet, where, when, fact}
 ) {
@@ -14,40 +19,81 @@ const dinoFactory = function(
   let _when = when;
   let _fact = fact;
 
+  /**
+   * @description Gets the Object type
+   * @returns {string}
+   */
   function getType() {
     return _type;
   }
 
+  /**
+   * @description Gets the species
+   * @returns {string}
+   */
   function getSpecies() {
     return _species;
   }
 
+  /**
+   * @description Gets the fact
+   * @returns {string}
+   */
   function getFact() {
     return _fact;
   }
 
+  /**
+   * @description Sets the fact
+   * @param {string} fact
+   */
   function setFact(fact) {
     _fact = fact;
   }
 
+  /**
+   * @description Gets the height
+   * @returns {string}
+   */
   function getHeight() {
     return _height;
   }
 
+  /**
+   * @description Gets the weight
+   * @returns {string}
+   */
   function getWeight() {
     return _weight;
   }
 
+  /**
+   * @description Gets the diet
+   * @returns {string}
+   */
   function getDiet() {
     return _diet;
   }
 
+  /**
+   * @description Creates the object containing the shared public methods to be used as the prototype
+   * @returns {Object} - the prototype object
+   */
   function createDino() {
     return {
+      /**
+       * @description Gets the Object type
+       * @returns {string}
+       */
       getType: function () {
         return getType();
       },
 
+      /**
+       * @description Sets the fact for Dinosaurs not of species Pigeon
+       * @param {string} fact
+       * @returns {string} - the status of the operation
+       */
       setFact: function (fact) {
         if (getSpecies() === "Pigeon") {
           return "Pigeon fact cannot be changed!";
@@ -58,6 +104,10 @@ const dinoFactory = function(
         }
       },
 
+      /**
+       * @description Generate a string representing the HTML element of the Dinosaur
+       * @returns {string}
+       */
       generateElementString: function () {
         return `
         <h2>${getSpecies()}</h2>
@@ -66,19 +116,38 @@ const dinoFactory = function(
     `;
       },
 
+      /**
+       * @description Compares the weight of the Dinosaur to the weight of the Human
+       * @param {Object} human - An Object representing the Human
+       * @returns {string} - The result of the comparison
+       */
       compareWeight: function (human) {
         return `You are ${human.weight < getWeight() ? "lighter" : "heavier"} than ${getSpecies()}`;
       },
 
+      /**
+       * @description Compares the height of the Dinosaur to the height of the Human
+       * @param {Object} human - An Object representing the Human
+       * @returns {string} - The result of the comparison
+       */
       compareHeight: function (human) {
         return `You are ${human.getHeightInInches() < getHeight() ? "shorter" : "taller"} 
       than ${getSpecies()}`;
       },
 
+      /**
+       * @description Compares the diet of the Dinosaur to the diet of the Human
+       * @param {Object} human - An Object representing the Human
+       * @returns {string} - The result of the comparison
+       */
       compareDiet: function (human) {
         return `You and ${getSpecies()} have ${getDiet() === human.diet.toLowerCase() ? "the same" : "a different"} diet`;
       },
 
+      /**
+       * @description Gets the list of methods that should be subject to a random call
+       * @returns {Array} - The methods on the prototype object
+       */
       getMethodList: function() {
         return [
           createDino().compareHeight,
